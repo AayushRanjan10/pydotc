966dcf3bcbce8cea378b39bb4de518f8712eff6a
AayushRanjan10 <aayushranjan103@gmail.com>
2026-04-17 15:36:49 +0530

    Lexer implementation

---
+++ src/lexer.c
@@ -0,0 +1,256 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "lexer.h"
+
+static const char* src;
+static int pos = 0;
+
+static int indent_stack[100] = {0}; 
+static int stack_top = 0;
+
+static Token peeked_token;
+static int has_peeked = 0;
+
+void init_lexer(const char* source_code) {
+    src = source_code;
+    pos = 0;
+
+    stack_top = 0;
+    indent_stack[0] = 0;
+
+    has_peeked = 0;
+}
+
+Token peek_next_token() {
+    if (!has_peeked) {
+        peeked_token = get_next_token();
+        has_peeked = 1;
+    }
+    return peeked_token;
+}
+
+Token get_next_token() {
+
+    if (has_peeked) {
+        has_peeked = 0;
+        return peeked_token;
+    }
+
+    Token tok;
+
+    while (src[pos] != '\0') {
+        char c = src[pos];
+
+        // If there is a space or tab in the middle of a line, we can ignore it
+        if (c==' ' || c=='\t') {
+            pos++;
+            continue;
+        }
+
+        // Starting a new line
+        if (c=='\n') {
+            pos++;
+            int spaces = 0;
+            while (src[pos] == ' ' || src[pos] == '\t') {
+                if (src[pos] == ' ') {
+                    spaces += 1;
+                } else if (src[pos] == '\t') {
+                    spaces += 4;
+                }
+                pos++;
+            }
+            if (spaces>indent_stack[stack_top]) {
+                stack_top++;
+                indent_stack[stack_top] = spaces;
+                tok.type = INDENT;
+                strcpy(tok.value, "INDENT");
+                return tok;
+            }
+            else if (spaces<indent_stack[stack_top]) {
+                indent_stack[stack_top] = 0;
+                stack_top--;
+                tok.type = DEDENT;
+                strcpy(tok.value, "DEDENT");
+                return tok;
+            }
+            else {
+                tok.type = NEWLINE;
+                strcpy(tok.value, "NEWLINE");
+                return tok;
+            }
+        }
+
+        // Single character tokens
+        if (c=='+') {
+            tok.type = PLUS;
+            strcpy(tok.value, "+");
+            pos++;
+            return tok;
+        }
+        if (c=='-') {
+            tok.type = MINUS;
+            strcpy(tok.value, "-");
+            pos++;
+            return tok;
+        }
+        if (c=='(') {
+            tok.type = LPAREN;
+            strcpy(tok.value, "(");
+            pos++;
+            return tok;
+        }
+        if (c==')') {
+            tok.type = RPAREN;
+            strcpy(tok.value, ")");
+            pos++;
+            return tok;
+        }
+
+        // Characters which may be double-character tokens
+        if (c=='*') {
+            if (src[pos+1]=='*') {
+                tok.type = POWER;
+                strcpy(tok.value, "**");
+                pos = pos+2;
+                return tok;
+            }
+            else {
+                tok.type = STAR;
+                strcpy(tok.value, "*");
+                pos++;
+                return tok;
+            }
+        }
+        else if (c=='/') {
+            if (src[pos+1]=='/') {
+                tok.type = FLOORDIV;
+                strcpy(tok.value, "//");
+                pos = pos+2;
+                return tok;
+            }
+            else {
+                tok.type = SLASH;
+                strcpy(tok.value, "/");
+                pos++;
+                return tok;
+            }
+        }
+        else if (c == '<') {
+            if (src[pos+1] == '=') {
+                tok.type = LTE;
+                strcpy(tok.value, "<=");
+                pos += 2;
+                return tok;
+            } 
+            else {
+                tok.type = LT;
+                strcpy(tok.value, "<");
+                pos++;
+                return tok;
+            }
+        }
+        else if (c == '>') {
+            if (src[pos+1] == '=') {
+                tok.type = GTE;
+                strcpy(tok.value, ">=");
+                pos += 2;
+                return tok;
+            } 
+            else {
+                tok.type = GT;
+                strcpy(tok.value, ">");
+                pos++;
+                return tok;
+            }
+        }
+        else if (c == '=') {
+            if (src[pos+1] == '=') {
+                tok.type = ASSIGN; 
+                strcpy(tok.value, "==");
+                pos += 2;
+                return tok;
+            } 
+            else {
+                tok.type = ASSIGN;
+                strcpy(tok.value, "=");
+                pos++;
+                return tok;
+            }
+        }
+        else if (c == '!') {
+            if (src[pos+1] == '=') {
+                tok.type = NOTEQUAL; 
+                strcpy(tok.value, "!=");
+                pos += 2;
+                return tok;
+            } 
+            else {
+                // In Python, a '!' without an '=' is a syntax error
+                tok.type = ERROR; 
+                strcpy(tok.value, "!");
+                pos++;
+                return tok;
+            }
+        }
+
+        // Numbers
+        if (isdigit(src[pos])) {
+            tok.type = NUMBER;
+            char num[20];
+            char *curr = num;
+            while (isdigit(src[pos]) || src[pos]=='.') {
+                *curr = src[pos];
+                curr++;
+                pos++;
+            }
+            *curr = '\0';
+            strcpy(tok.value, num);
+            return tok;
+        }
+        
+        // Strings
+        if (src[pos]=='"') {
+            tok.type = STRING;
+            char str[1000];
+            char *curr = str;
+            pos++;
+            while (src[pos]!='"') {
+                *curr = src[pos];
+                curr++;
+                pos++;  
+            }
+            *curr = '\0';
+            strcpy(tok.value, str);
+            return tok;
+        }
+
+        // Keywords and identifiers
+        if (isalpha(src[pos]) || src[pos] == '_') {
+            char id[1000];
+            char *curr = id;
+            pos++;
+            while (src[pos]!=' ') {
+                *curr = src[pos];
+                curr++;
+                pos++;  
+            }
+            *curr = '\0';
+            if (strcmp(tok.value, "print") == 0) tok.type = PRINT;
+            else if (strcmp(tok.value, "while") == 0) tok.type = WHILE;
+            else if (strcmp(tok.value, "if") == 0) tok.type = IF;
+            else if (strcmp(tok.value, "elif") == 0) tok.type = ELIF;            
+            else if (strcmp(tok.value, "else") == 0) tok.type = ELSE;
+            else tok.type = IDENTIFIER;
+            strcpy(tok.value, id);
+            return tok;
+        }
+
+    }
+
+    // End of File
+    tok.type = END_OF_FILE;
+    strcpy(tok.value, "EOF");
+    return tok;
+}
---
+++ src/lexer.h
@@ -0,0 +1,50 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+typedef enum {
+    PLUS,
+    MINUS,
+    STAR,
+    SLASH,
+    MODULUS,
+    POWER,
+    FLOORDIV,
+    ASSIGN,
+    LPAREN,
+    RPAREN,
+    LT,
+    GT,
+    LTE,
+    GTE,
+    NOTEQUAL,
+
+    PRINT,
+    WHILE,
+    IF,
+    ELIF,
+    ELSE,
+
+    NUMBER,
+    IDENTIFIER,
+    STRING,
+
+    NEWLINE,
+    INDENT,
+    DEDENT,
+
+    ERROR,
+
+    END_OF_FILE
+} TokenType;
+
+
+typedef struct {
+    TokenType type;
+    char value[1000];
+} Token;
+
+void init_lexer(const char* source_code);
+Token get_next_token();
+Token peek_next_token(); // Needed for parser logic, to check next token without consuming it
+
+#endif
